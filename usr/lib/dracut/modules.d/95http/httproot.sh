0979dbe54a1a254b81f1bd0376c1d012dc51d102
Jura <jurashka@gmail.com>
2022-02-09 17:34:33 +0300

    Newer curl expect --output should be used.

--- usr/lib/dracut/modules.d/95http/httproot.sh
+++ usr/lib/dracut/modules.d/95http/httproot.sh
@@ -11,8 +11,8 @@ NEWROOT="$3"
 
 echo URL: $root
 
-mount -t tmpfs -o size=10g,nodiratime,noatime tmpfs $NEWROOT || error
-curl -s $root | pigz -d | tar -x -f - -C $NEWROOT || error
+mount -t tmpfs -o size=10g,nodiratime,noatime tmpfs $NEWROOT || error 
+curl -s $root -o - | pigz -d | tar -x -f - -C $NEWROOT || error
 
 # inject new exit_if_exists
 echo 'settle_exit_if_exists="--exit-if-exists=/dev/root"; rm -f -- "$job"' > $hookdir/initqueue/http.sh
